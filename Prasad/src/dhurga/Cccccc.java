a1aa18913cfa68d2cbec2c3ccd00cf9d6ffb9a1f
Dhurga <Dhurga@admin-PC>
2019-02-13 15:51:03 +0530

    dhurga as updated

---
+++ Prasad/src/dhurga/Cccccc.java
@@ -0,0 +1,18 @@
+package dhurga;
+
+public class Cccccc {
+public  void m1() {
+	System.out.println("i love my self");
+}
+public  static void m2() {
+	System.out.println("i love india");
+}
+	public static void main(String[] args) {
+		
+    Cccccc obj=new Cccccc();
+    
+    		obj.m1();
+    		m2();
+	}
+
+}
