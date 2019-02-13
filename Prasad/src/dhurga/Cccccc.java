ffcb5064b9cb0b546a92be43dd4d2ee9c6b5a628
Dhurga <Dhurga@admin-PC>
2019-02-13 16:00:02 +0530

    dhurga updated

--- Prasad/src/dhurga/Cccccc.java
+++ Prasad/src/dhurga/Cccccc.java
@@ -4,15 +4,14 @@ public class Cccccc {
 public  void m1() {
 	System.out.println("i love my self");
 }
-public  static void m2() {
-	System.out.println("i love india");
-}
+
+
 	public static void main(String[] args) {
 		
     Cccccc obj=new Cccccc();
     
     		obj.m1();
-    		m2();
+    		
 	}
 
 }
